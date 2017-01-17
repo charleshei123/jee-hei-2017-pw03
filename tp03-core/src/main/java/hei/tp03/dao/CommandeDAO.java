c14ff20f3b486b65394416c0ddd6e912a0ec43d7
charleshei123 <charles.douville-de-franssu@hei.fr>
2017-01-17 10:41:14 +0100

    commit commande DAO

--- tp03-core/src/main/java/hei/tp03/dao/CommandeDAO.java
+++ tp03-core/src/main/java/hei/tp03/dao/CommandeDAO.java
@@ -1,11 +1,10 @@
 package hei.tp03.dao;
 
-import hei.tp03.entity.Produit;
+import hei.tp03.entity.Commande;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by N0stress on 17/01/2017.
  */
-public interface ProduitDAO extends JpaRepository<Produit, Long>  {
-
+public interface CommandeDAO extends JpaRepository<Commande, Long> {
 }
